8bbc8c255fdd0e2883760ef02b29d84f7b34240b
manojmarvels <manojmarvels0@gmail.com>
2024-11-04 23:44:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,14 +1,1 @@
-def gitdownload(repo)
-{
-   git "https://github.com/manojmarvels/${repo}.git"
-}
-
-def build()
-{
-  sh 'mvn package'
-}
-def deploy(jobame,ip,appname)
-{
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war   ubuntu@${ip}:8080 /var/lib/tomcat9/webapps/${appname}.war"
-}
 
